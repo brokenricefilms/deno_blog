120f5e905ff4209d98783c24dc206f49087e1ce3
Thuan Pham <phamtanthuan2311@gmail.com>
2022-10-07 06:08:11 +0700

    init

---
+++ main.js
@@ -0,0 +1,17 @@
+import blog, { ga } from "https://deno.land/x/blog/blog.tsx";
+
+blog({
+  author: "Thuan Pham",
+  title: "Thuan Pham",
+  description: "Random nerd (Front-end Developer/Creator)",
+  avatar: "avata.jpeg",
+  avatarClass: "rounded-full",
+  links: [
+    { title: "Email", url: "mailto:phamtanthuan2311@gmail.com" },
+    { title: "GitHub", url: "https://github.com/thuanpham2311" },
+    { title: "Links", url: "https://bio.link/thuanpham2311" },
+  ],
+  lang: "en",
+  dateStyle: "long",
+  middlewares: [ga("G-4V1R393JHN")],
+});
