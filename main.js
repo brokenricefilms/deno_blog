ea6b114caf59a73e4b908792c3a42c5b7bd15ba0
Thuan Pham <thuanowa@gmail.com>
2023-01-18 12:44:05 +0700

    fix: google analytic

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-import blog, { ga } from "https://deno.land/x/blog@0.5.0/blog.tsx";
+import blog, { ga, redirects } from "https://deno.land/x/blog/blog.tsx";
 
 blog({
   author: "Thuan Pham",
   title: "Thuan Pham",
-  description: "thuanowa blog, thuanowa, front-end dev",
+  description: "Yo! I'm a random nerd, front-end dev, and creator,...",
   avatar: "profile_picture.png",
   avatarClass: "rounded-full",
   links: [
     { title: "Email", url: "mailto:thuanowa@gmail.com" },
     { title: "GitHub", url: "https://github.com/thuanOwa" },
-    { title: "Links", url: "https://bio.link/thuanOwa" },
+    { title: "link in bio/ links", url: "https://bio.link/thuanOwa" },
   ],
   dateFormat: (date) =>
     new Intl.DateTimeFormat("en-GB", { dateStyle: "long" }).format(date),
   lang: "en",
-  middlewares: [ga("G-4V1R393JHN")],
+  middlewares: [ga("G-4V1R393JHN"), redirects({})],
 });
